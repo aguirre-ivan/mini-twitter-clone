26c7dbed0e573b2f26a9fd35bc9d3acf9410f01b
aguirre-ivan <iaguirre@fi.uba.ar>
2024-03-16 18:14:53 -0300

    Added conneciton status method

--- helpers/database.php
+++ helpers/database.php
@@ -7,9 +7,14 @@ class Database {
         $dsn = "mysql:host=$host;dbname=$db_name";
         $this->pdo = new PDO($dsn, $user, $password);
         $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo "Conexión exitosa";
     }
 
     public function getPdo() {
         return $this->pdo;
+    }
+
+    public function getConnectionStatus() {
+        return $this->pdo->getAttribute(PDO::ATTR_CONNECTION_STATUS);
     }
 }
