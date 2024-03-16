794a8da0206b9ddfaef9055c6bf9ff166fa2a01c
aguirre-ivan <iaguirre@fi.uba.ar>
2024-03-16 18:09:22 -0300

    Fixed database

---
+++ helpers/config.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once('database.php');
+
+define('ENV_FILE_PATH', __DIR__ . '/../.env');
+
+if (!file_exists(ENV_FILE_PATH)) {
+    die("El archivo .env no existe" . ENV_FILE_PATH);
+}
+
+$env_vars = parse_ini_file(ENV_FILE_PATH);
+
+$db_host = $env_vars['DB_HOST'];
+$db_name = $env_vars['DB_NAME'];
+$db_user = $env_vars['DB_USER'];
+$db_pass = $env_vars['DB_PASS'];
+
+$database = new Database($db_host, $db_name, $db_user, $db_pass);
+$pdo = $database->getPdo();
