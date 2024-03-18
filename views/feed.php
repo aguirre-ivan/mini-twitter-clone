a4b2256cf0c77f49d4180ca4a363f02f0da03202
aguirre-ivan <iaguirre@fi.uba.ar>
2024-03-18 01:29:06 -0300

    Added container to feed

--- views/feed.php
+++ views/feed.php
@@ -2,7 +2,10 @@
 <body>
     <?php require_once('partials/header.php') ?>
 
-    <?php require_once('tweet/add_form.php') ?>
+    <div class="feed-page-container container">
+        <?php require_once('tweet/add_form.php') ?>
+        <?php require_once('tweet/all.php') ?>
+    </div>
     
     <?php require_once('partials/footer.html') ?>
 </body>
