ea8e275ea832c5c89a4523addb350f5d10976def
aguirre-ivan <iaguirre@fi.uba.ar>
2024-03-24 22:46:27 -0300

    Added left meenu file

---
+++ app/views/partials/left_menu.php
@@ -0,0 +1,29 @@
+<nav class="nav-sidebar-menu">
+    <ul class="sidebar-menu">
+        <li>
+            <a href="/index">
+                <i class="fa-solid fa-house"></i>
+                <div class="sidebar-menu__item-text">Home</div>
+            </a>
+        </li>
+        <li>
+            <a href="/user/explore">
+                <i class="fa-solid fa-magnifying-glass"></i>
+                <div class="sidebar-menu__item-text">A quien seguir</div>
+            </a>
+        </li>
+        <li>
+            <a href="/user/profile">
+                <i class="fa-solid fa-user">
+                </i>
+                <div class="sidebar-menu__item-text">Perfil</div>
+            </a>
+        </li>
+        <li>
+            <a href="/user/logout">
+                <i class="fa-solid fa-right-from-bracket"></i>
+                <div class="sidebar-menu__item-text">Desconectarse</div>
+            </a>
+        </li>
+    </ul>
+</nav>
