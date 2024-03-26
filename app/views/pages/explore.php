5198a03ba11b77d6e1da5f31a6903434b0cf4ead
aguirre-ivan <iaguirre@fi.uba.ar>
2024-03-25 23:01:02 -0300

    Extended explore to partials

--- app/views/pages/explore.php
+++ app/views/pages/explore.php
@@ -1,11 +1,4 @@
-<?php if (!empty($page_data['users'])) : ?>
-    <div class="users-container">
-        <?php foreach ($page_data['users'] as $user) : ?>
-            <div class="user">
-                <a href="/user/profile/<?= $user['id'] ?>" class="user__username"><?= $user['username'] ?></a>
-            </div>
-        <?php endforeach; ?>
-    </div>
-<?php else : ?>
-    <div class="empty-users-container">Aun no hay usuarios.</div>
-<?php endif; ?>
+
+<?php require_once APP . '/views/partials/head.php' ?>
+<?php require_once APP . '/views/partials/user/explore.php' ?>
+<?php require_once APP . '/views/partials/footer.php' ?>
