9f9a704c7de0df7e34d135a58d52b953487c9949
aguirre-ivan <iaguirre@fi.uba.ar>
2024-03-25 21:57:44 -0300

    Added variables and sidebar menu styles

--- app/views/partials/left_menu.php
+++ app/views/partials/left_menu.php
@@ -2,27 +2,34 @@
     <ul class="sidebar-menu">
         <li>
             <a href="/index">
-                <i class="fa-solid fa-house"></i>
-                <div class="sidebar-menu__item-text">Home</div>
+                <div class="link-wrapper">
+                    <i class="fa-solid fa-house"></i>
+                    <div class="sidebar-menu__item-text">Home</div>
+                </div>
             </a>
         </li>
         <li>
             <a href="/user/explore">
-                <i class="fa-solid fa-magnifying-glass"></i>
-                <div class="sidebar-menu__item-text">A quien seguir</div>
+                <div class="link-wrapper">
+                    <i class="fa-solid fa-magnifying-glass"></i>
+                    <div class="sidebar-menu__item-text">A quien seguir</div>
+                </div>
             </a>
         </li>
         <li>
             <a href="/user">
-                <i class="fa-solid fa-user">
-                </i>
-                <div class="sidebar-menu__item-text">Perfil</div>
+                <div class="link-wrapper">
+                    <i class="fa-solid fa-user"></i>
+                    <div class="sidebar-menu__item-text">Perfil</div>
+                </div>
             </a>
         </li>
         <li>
             <a href="/user/logout">
-                <i class="fa-solid fa-right-from-bracket"></i>
-                <div class="sidebar-menu__item-text">Desconectarse</div>
+                <div class="link-wrapper">
+                    <i class="fa-solid fa-right-from-bracket"></i>
+                    <div class="sidebar-menu__item-text">Desconectarse</div>
+                </div>
             </a>
         </li>
     </ul>
