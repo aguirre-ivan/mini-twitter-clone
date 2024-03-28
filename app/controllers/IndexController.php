a5dc26df58739e8dcf4ab260d4f32146fe7634fd
Ivan Aguirre <74165953+aguirre-ivan@users.noreply.github.com>
2024-03-28 01:34:42 -0300

    Feature/feed all tweets (#9)
    * Added feed and all tweets views

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -2,12 +2,16 @@
 
 class IndexController extends Controller
 {
-    public function index()
+    public function index($params = [])
     {
         $this->isIndexInUrl();
+        $this->redirect('/index/feed');
+    }
 
+    public function feed()
+    {
         if (!isset($_SESSION['user_id'])) {
-            $this->loadView('index', ['title' => 'Bienvenido a Twitter']);
+            $this->redirect('index');
         } else {
             $this->loadModel('Tweet');
             $tweet = new Tweet();
@@ -15,7 +19,20 @@ class IndexController extends Controller
             $this->loadController('TweetController');
             $tweetController = new TweetController();
             $tweets = $tweetController->allTweets();
-            $this->loadView('feed', ['title' => 'Inicio', 'tweets' => $tweets]);
+            $this->loadView('feed', ['title' => 'Inicio / Twitter', 'tweets' => $tweets]);
+        }
+    }
+
+    public function following()
+    {
+        if (!isset($_SESSION['user_id'])) {
+            $this->redirect('index');
+        } else {
+            $this->loadModel('Tweet');
+            $this->loadController('TweetController');
+            $tweetController = new TweetController();
+            $tweets = $tweetController->followingTweets($_SESSION['user_id']);
+            $this->loadView('feed', ['title' => 'Inicio / Twitter', 'tweets' => $tweets]);
         }
     }
 }
