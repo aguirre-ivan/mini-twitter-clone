f42525c92c4f14e44295ae4f08321e8f25e77288
aguirre-ivan <aguirre.ivang@gmail.com>
2024-04-16 18:30:42 -0300

    Added general docs

--- app/init.php
+++ app/init.php
@@ -1,12 +1,15 @@
 <?php
 
+// Load configuration settings and helper functions
 require_once 'config/config.php';
 require_once 'helpers/functions.php';
 
+// Start a session if not already started
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
 
+// Autoload classes using spl_autoload_register
 spl_autoload_register(function ($lib) {
     require_once 'lib/' . $lib . '.php';
 });
