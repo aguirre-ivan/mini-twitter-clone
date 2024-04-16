30ef9486f7cf6baaf5edeca10fae9770cfd7698f
aguirre-ivan <iaguirre@fi.uba.ar>
2024-04-16 02:24:04 -0300

    Added default images

--- app/config/config.php
+++ app/config/config.php
@@ -2,8 +2,11 @@
 
 define('APP', dirname(__DIR__));
 
-define('UPLOAD_IMG_DIRECTORY', '/files/images/');
+define('UPLOAD_IMG_DIRECTORY', realpath(APP . '/../public/files/images/') . '/');
 define('MAX_UPLOAD_SIZE_MB', 10);
+
+define('DEFAULT_HEADER_IMG_PATH', 'default/default_header.jpg');
+define('DEFAULT_PROFILE_IMG_PATH', 'default/default_profile.jpg');
 
 define('DB_HOST', 'mariadb');
 define('DB_NAME', 'mini_twitter');
--- app/views/partials/user/profile_container.php
+++ app/views/partials/user/profile_container.php
@@ -1,4 +1,17 @@
+<?php
+
+$header_image = $page_data['user_data']['header_image'] ?? DEFAULT_HEADER_IMG_PATH;
+$profile_image = $page_data['user_data']['profile_image'] ?? DEFAULT_PROFILE_IMG_PATH;
+
+?>
+
 <div class="profile-container">
+    <div class="profile-container__header-image">
+        <img src="/files/images/<?= $header_image ?>">
+    </div>
+    <div class="profile-container__profile-image">
+        <img src="/files/images/<?= $profile_image ?>">
+    </div>
     <div class="profile-container__name"><?= $page_data['user_data']['name'] ?></div>
     <div class="profile-container__username"><?= $page_data['user_data']['username'] ?></div>
     <a class="profile-container__button" href="<?= $page_data['profile_button']['link'] ?>" class="profile-container__edit-link">
