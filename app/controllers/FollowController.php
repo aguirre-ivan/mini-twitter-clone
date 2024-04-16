e5c995b2213d618e2cc48659e0656cb2ae7dc3a2
aguirre-ivan <aguirre.ivang@gmail.com>
2024-04-16 18:26:44 -0300

    Added FollowController docs

--- app/controllers/FollowController.php
+++ app/controllers/FollowController.php
@@ -1,26 +1,50 @@
 <?php
 
+/**
+ * FollowController
+ *
+ * This controller manages actions related to following and unfollowing users,
+ * as well as retrieving followers and following lists.
+ */
 class FollowController extends Controller
 {
     private $user;
     private $userLogged;
     private $userLoggedData;
 
+    /**
+     * Sets the user ID being followed.
+     *
+     * @param int $user The ID of the user being followed.
+     */
     public function setUser($user)
     {
         $this->user = $user;
     }
 
+    /**
+     * Sets the ID of the logged-in user.
+     *
+     * @param int $userLogged The ID of the logged-in user.
+     */
     public function setUserLogged($userLogged)
     {
         $this->userLogged = $userLogged;
     }
 
+    /**
+     * Sets data for the logged-in user.
+     *
+     * @param array $userLoggedData Data of the logged-in user.
+     */
     public function setUserLoggedData($userLoggedData)
     {
         $this->userLoggedData = $userLoggedData;
     }
 
+    /**
+     * Follows a user.
+     */
     public function follow()
     {
         if ($this->userLogged && $this->userLogged != $this->user) {
@@ -31,6 +55,9 @@ class FollowController extends Controller
         }
     }
 
+    /**
+     * Unfollows a user.
+     */
     public function unfollow()
     {
         if ($this->userLogged && $this->userLogged != $this->user) {
@@ -41,6 +68,9 @@ class FollowController extends Controller
         }
     }
 
+    /**
+     * Retrieves followers of a user.
+     */
     public function followers()
     {
         $this->loadModel('Follow');
@@ -50,6 +80,9 @@ class FollowController extends Controller
         $this->loadView('follows', ['title' => 'Seguidores de ' . $this->userLoggedData['username'], 'users' => $followers, 'user_data' => $user_data]);
     }
 
+    /**
+     * Retrieves users followed by a user.
+     */
     public function following()
     {
         $this->loadModel('Follow');
@@ -59,6 +92,9 @@ class FollowController extends Controller
         $this->loadView('follows', ['title' => 'Personas que sigue ' . $this->userLoggedData['username'], 'users' => $following, 'user_data' => $user_data]);
     }
 
+    /**
+     * Retrieves users not followed by the logged-in user.
+     */
     public function notFollowing()
     {
         $this->loadModel('Follow');
@@ -67,6 +103,11 @@ class FollowController extends Controller
         $this->loadView('explore', ['title' => 'Explorar', 'users' => $notFollowing]);
     }
 
+    /**
+     * Loads user data for display.
+     *
+     * @return array User data.
+     */
     private function loadUserHeader()
     {
         $this->loadModel('User');
