6c15559d2ae42f0ddbb5f03fd568595f1ba71a02
Ivan Aguirre <74165953+aguirre-ivan@users.noreply.github.com>
2024-04-16 18:46:55 -0300

    Docs/controllers (#12)
    * Added TweetController docs
    * Added UserController form
    * Added IndexController docs
    * Added FollowController docs
    * Added Controller docs

--- app/lib/Controller.php
+++ app/lib/Controller.php
@@ -1,19 +1,42 @@
 <?php
 
+/**
+ * Controller
+ *
+ * This class provides common methods and utilities for controllers.
+ */
 class Controller
 {
+    /**
+     * Loads a model.
+     *
+     * @param string $model The name of the model to load.
+     * @return object An instance of the loaded model.
+     */
     public function loadModel($model)
     {
         require_once '../app/models/' . $model . '.php';
         return new $model;
     }
 
+    /**
+     * Loads a controller.
+     *
+     * @param string $controller The name of the controller to load.
+     * @return object An instance of the loaded controller.
+     */
     public function loadController($controller)
     {
         require_once '../app/controllers/' . $controller . '.php';
         return new $controller;
     }
 
+    /**
+     * Loads a view.
+     *
+     * @param string $view The name of the view to load.
+     * @param array $page_data Data to pass to the view.
+     */
     public function loadView($view, $page_data = [])
     {
         $view_file = '../app/views/pages/' . $view . '.php';
@@ -24,17 +47,67 @@ class Controller
         }
     }
 
+    /**
+     * Redirects to a specified location.
+     *
+     * @param string $location The URL to redirect to.
+     */
+    public function redirect($location)
+    {
+        header("Location: $location");
+    }
+
+    /**
+     * Displays a "Page Not Found" error page and terminates the script.
+     */
     public function notFound()
     {
         require_once '../app/views/pages/page_not_found.php';
         die();
     }
 
-    public function redirect($location)
+    /**
+     * Handles access restriction for specific controller methods.
+     *
+     * @param string $class_name The name of the controller class.
+     * @param string $method_name The name of the controller method.
+     */
+    public function handleUrlAccessRestriction($class_name, $method_name)
     {
-        header("Location: $location");
+        $controller = strtolower(str_replace('Controller', '', $class_name));
+
+        if ($this->getUrl() == [$controller, $method_name]) {
+            $this->notFound();
+        }
     }
 
+    /**
+     * Checks if 'index' is present in the URL and displays a "Page Not Found" error page if it is.
+     */
+    public function isIndexInUrl()
+    {
+        if (isset($this->getUrl()[1]) && $this->getUrl()[1] == 'index') {
+            $this->notFound();
+        }
+    }
+
+    /**
+     * Asserts the number of parameters in the URL and displays a "Page Not Found" error page if incorrect.
+     *
+     * @param int $amount The expected number of parameters in the URL.
+     */
+    public function assertParamsAmount($amount)
+    {
+        if (count($this->getUrl()) != $amount + 2) {
+            $this->notFound();
+        }
+    }
+
+    /**
+     * Parses and sanitizes the URL.
+     *
+     * @return array An array containing the parsed and sanitized URL segments.
+     */
     private function getUrl()
     {
         if (isset($_GET['url'])) {
@@ -44,26 +117,6 @@ class Controller
             return $url;
         } else {
             return [];
-        }
-    }
-
-    public function handleUrlAccessRestriction ($class_name, $method_name) {
-        $controller = strtolower(str_replace('Controller', '', $class_name));
-
-        if ($this->getUrl() == [$controller, $method_name]) {
-            $this->notFound();
-        }
-    }
-
-    public function isIndexInUrl() {
-        if (isset($this->getUrl()[1]) and $this->getUrl()[1] == 'index') {
-            $this->notFound();
-        }
-    }
-
-    public function assertParamsAmount($amount) {
-        if (count($this->getUrl()) != $amount + 2) {
-            $this->notFound();
         }
     }
 }
