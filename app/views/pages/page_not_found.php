d752817b6def6fda129830b7ae80c70a61b1eab1
aguirre-ivan <aguirre.ivang@gmail.com>
2024-04-18 18:43:54 -0300

    Added title to page not found

--- app/views/pages/page_not_found.php
+++ app/views/pages/page_not_found.php
@@ -1,5 +1,6 @@
 <?php
 
+$page_data['title'] = 'Pagina no encontrada / Twitter';
 $user_logged_in = isset($_SESSION['user_id']);
 $page_not_found_class = $user_logged_in ? 'page-not-found--logged-in' : 'page-not-found--logged-out';
 
@@ -21,9 +22,9 @@ $page_not_found_class = $user_logged_in ? 'page-not-found--logged-in' : 'page-no
                 </a>
             </div>
         <?php endif; ?>
-        <h1>404 - Page Not Found</h1>
-        <p>The page you are looking for does not exist.</p>
-        <p>Click <a href="/">here</a> to go back to the home page.</p>
+        <h1>404 - Página no encontrada</h1>
+        <p>La página que estas buscando no se encuentra.</p>
+        <p>Clickeá <a href="/">acá</a> para volver a la página principal.</p>
     </div>
     <?php if ($user_logged_in) : ?>
         <div class="page-not-found__sidebar-second sidebar-second">
