b24739cc71720c77be80684683ad4dbc0dccece1
aguirre-ivan <iaguirre@fi.uba.ar>
2024-04-19 01:56:24 -0300

    Added edit form structure

--- app/views/pages/edit_profile.php
+++ app/views/pages/edit_profile.php
@@ -1,38 +1,43 @@
 <?php require_once APP . '/views/partials/head.php' ?>
 
-<?php var_dump($page_data['errors']); ?>
+<!-- <?php var_dump($page_data['errors']); ?> -->
 
 <div class="user-logged-in-page edit-profile-page">
     <div class="user-logged-in-page__sidebar-first sidebar-first">
         <?php require_once APP . '/views/partials/left_menu.php' ?>
     </div>
     <div class="user-logged-in-page__main-container">
-        <h1>Editar perfil</h1>
+        <div class="back-profile-link">
+            <a href="/user">
+                <i class="fa-solid fa-arrow-left"></i>
+            </a>
+            <h1>Editar perfil</h1>
+        </div>
         <div class="edit-profile-form">
             <form action="" method="POST" enctype="multipart/form-data">
                 <div class="edit-profile-form__item-container">
                     <label for="name">Nombre</label>
-                    <input type="text" id="name" value="<?= $page_data['user_data']['name'] ?>" name="name">
+                    <input class="twitter-input" type="text" id="name" value="<?= $page_data['user_data']['name'] ?>" name="name">
                 </div>
 
                 <div class="edit-profile-form__item-container">
                     <label for="location">Ubicación</label>
-                    <input type="text" id="location" value="<?= $page_data['user_data']['location'] ?>" name="location">
+                    <input class="twitter-input" type="text" id="location" value="<?= $page_data['user_data']['location'] ?>" name="location">
                 </div>
 
                 <div class="edit-profile-form__item-container">
                     <label for="headerImage">Foto de portada</label>
-                    <input type="file" id="headerImage" name="headerImage">
+                    <input class="twitter-input" type="file" id="headerImage" name="headerImage">
                 </div>
 
                 <div class="edit-profile-form__item-container">
                     <label for="profileImage">Foto de Perfil</label>
-                    <input type="file" id="profileImage" name="profileImage">
+                    <input class="twitter-input" type="file" id="profileImage" name="profileImage">
                 </div>
 
                 <div class="edit-profile-form__item-container">
                     <label for="bio">Biografía</label>
-                    <textarea id="bio" name="bio" rows="4" cols="50"><?= $page_data['user_data']['bio'] ?></textarea>
+                    <textarea class="twitter-input" id="bio" name="bio" rows="4" cols="50"><?= $page_data['user_data']['bio'] ?></textarea>
                 </div>
 
                 <button class="twitter-btn twitter-btn--lightblue" type="submit">Guardar</button>
