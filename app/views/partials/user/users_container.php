620f49ecbb9e78c8f957d39c07539961c30036eb
aguirre-ivan <aguirre.ivang@gmail.com>
2024-04-19 17:38:05 -0300

    Added basic user container

--- app/views/partials/user/users_container.php
+++ app/views/partials/user/users_container.php
@@ -1,23 +1,27 @@
-<?php
-$profile_image = $page_data['user_data']['profile_image'] ?? DEFAULT_PROFILE_IMG_PATH;
-?>
-
-<?php if (!empty($page_data['users'])) : ?>
+<?php if (!empty($page_data['users'])): ?>
     <div class="users-container">
-        <?php foreach ($page_data['users'] as $user) : ?>
+        <?php foreach ($page_data['users'] as $user): ?>
+            <?php
+            $profile_image = $user['profile_image'] ?? DEFAULT_PROFILE_IMG_PATH;
+            ?>
             <div class="user-container">
                 <div class="user-container__image">
                     <img src="<?= IMG_DIRECTORY . $profile_image ?>">
                 </div>
                 <div class="user-container__body">
-                    <a href="/user/profile/<?= $user['id'] ?>" class="user__name"><?= $user['name'] ?></a>
-                    <div class="user__username"><?= $user['username'] ?></div>
-                    <div class="user__bio"><?= $user['bio'] ?></div>
+                    <div class="user-container__info">
+                        <a href="/user/profile/<?= $user['id'] ?>"
+                            class="user-container__name"><?= $user['name'] ?></a>
+                        <div class="user-container__username"><?= $user['username'] ?></div>
+                        <?php if (isset($user['bio'])): ?>
+                            <div class="user__bio"><?= $user['bio'] ?></div>
+                        <?php endif; ?>
+                    </div>
                 </div>
 
             </div>
         <?php endforeach; ?>
     </div>
-<?php else : ?>
+<?php else: ?>
     <div class="empty-users-container">Aun no hay usuarios.</div>
 <?php endif; ?>
